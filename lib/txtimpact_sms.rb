e2a21fc88d7cefd7bff91305ad36bb9179ab28b0
David Miani <davidmiani@gmail.com>
2011-08-16 13:04:39 +1000

    Added exception handling

--- lib/txtimpact_sms.rb
+++ lib/txtimpact_sms.rb
@@ -7,6 +7,9 @@ class TxtImpact
 
   class FailedAuthenticationError < Error; end
   class NotEnoughCreditsError < Error; end
+  class KeywordIsTakenError < Error; end
+  class AccountUpdateError  < Error; end
+  class CreditCardDeclinedError < Error; end
 
 
   class JobId
@@ -54,13 +57,19 @@ class TxtImpact
   # @param [String] text The message text
   # @param [String] batch_reference If sending to multiple numbers, is used to identify
   #   the send request used in reports
+  # @return [JobId, Integer] If a single sms is being sent, a JobId is returned. Otherwise
+  # an Integer for the BatchID is returned.
+  # @raise NotEnoughError Not enough credits to send the sms
+  # @raise FailedAuthenticationError some authentication details are wrong
   def send_sms(to_number, text, batch_reference = "batched send")
     params = common_options
     params['VERSION'] = '2.0'
     params['FROM'] = short_code
     params['TEXT'] = text
 
-    if to_number.is_a? Integer
+    batch_send = to_number.is_a? Integer
+
+    if batch_send
       params['TO'] = to_number
     else
       params['TO'] = to_number.join(',')
@@ -73,21 +82,38 @@ class TxtImpact
       when /^ERR: 301/
         raise FailedAuthenticationError
       when /^ERR: 305/
-        raise NotEnoughCredits
+        raise NotEnoughCreditsError
     end
-    JobId.from_s(res)
+    if (batch_send)
+      res.match(/BATCHID: \d+/)[1]
+    else
+      JobId.from_s(res)
+    end
   end
 
   # Adds some credits to the account
   # @param [Integer] keyword_credits The number of credits to add
+  # @return [void]
   def subscribe_credits(keyword_credits = 1)
     url = URI.parse('http://mzone.wire2air.com/mserver/api/subscribekeywords.aspx')
-    Net::HTTP.post_form(url, {
+    res = Net::HTTP.post_form(url, {
         'USERID' => userid,
         'PASSWORD' => password,
         'KEYWORDREDITS' => keyword_credits
 
     })
+    case res
+      when /^Err:70[34]/
+        raise ArgumentError.new "Missing username or password"
+      when /^Err:705/
+        raise KeywordIsTakenError
+      when /^Err:300/
+        raise FailedAuthenticationError
+      when /^Err:715/
+        raise AccountUpdateError
+      when /^Err:716/
+        raise CreditCardDeclinedError
+    end
 
   end
 
@@ -95,11 +121,13 @@ class TxtImpact
   # @return Integer the number of credits available
   def credit_count
     url = URI.parse('http://smsapi.wire2air.com/smsadmin/checksmscredits.aspx')
-    Net::HTTP.post_form(url, {
+    res = Net::HTTP.post_form(url, {
         'USERID' => userid,
         'PASSWORD' => password,
         'VASID' => vasid
     })
+    raise FailedAuthenticationError if res =~ /ERR: 301/
+    res.to_i
   end
 
   # Checks whether the keyword can be registered.
@@ -130,7 +158,20 @@ class TxtImpact
     params['STOPMSG'] = opts[:stop_msg]
     params['ACTION'] = 'ADD'
 
-    Net::HTTP.post_form(url, params)
+    res = Net::HTTP.post_form(url, params)
+
+    case res of
+      when /Err:70[012346789]/, /Err:71[0134]/
+        raise ArgumentError.new res
+      when /Err:705/
+        raise KeywordIsTakenError
+      when /Err:712/
+        raise "Sticky session is not allowed"
+    end
+
+    res.match(/SERVICEID:\d+/)[1].to_i
+
+    end
   end
 
   private
